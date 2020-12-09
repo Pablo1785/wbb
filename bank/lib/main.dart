734a2a53a086cd0f48789a5034cec8f429f97189
matwieclawek <mateusz.wieclawek@outlook.com>
2020-12-10 00:23:12 +0100

    nav betw homepage and loggedin

--- bank/lib/main.dart
+++ bank/lib/main.dart
@@ -1,7 +1,95 @@
 import 'package:bank/signin.dart';
+import 'package:bank/signup.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-void main() => runApp(SignInApp());
+void main() => runApp(HomeApp());
+
+class HomeApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Wirtualny Bank Bitcoinów',
+      theme: ThemeData(
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    var screenSize = MediaQuery.of(context).size;
+
+    return Scaffold(
+      //extendBodyBehindAppBar: true,
+      appBar: PreferredSize(
+        preferredSize: Size(screenSize.width, 1000),
+        child: Container(
+          color: Colors.blue,
+          child: Padding(
+            padding: EdgeInsets.all(20),
+            child: Row(
+              children: [
+                Text(
+                    'WBB',
+                    style: GoogleFonts.montserrat(
+                        fontSize: 15,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.white
+                    )
+                ),
+                //SizedBox(width: screenSize.width / 50),
+                Expanded(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
 
 
 
+
+
+                    ],
+                  ),
+                ),
+                InkWell(
+                  onTap: () {runApp(SignInApp());},
+                  child: Text(
+                      'Zaloguj się',
+                      style: GoogleFonts.montserrat(
+                          fontSize: 15,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.white
+                      )
+                  ),
+                ),
+                SizedBox(
+                  width: screenSize.width / 50,
+                ),
+                InkWell(
+                  onTap: () {runApp(SignUpApp());},
+                  child: Text(
+                      'Zarejestruj się',
+                      style: GoogleFonts.montserrat(
+                          fontSize: 15,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.white
+                      )
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+
+    );
+  }
+}
