a5f7c9bff8459d97f32ec06b069919ac419a4487
Stanisław Światłoch <sswiatloch@hotmail.com>
2020-12-10 20:16:12 +0100

    Registered models in admin

--- bank_backend/core/admin.py
+++ bank_backend/core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Profile, SubAccount, BankDeposit, Transaction, BitcoinTransaction
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(SubAccount)
+admin.site.register(BankDeposit)
+admin.site.register(Transaction)
+admin.site.register(BitcoinTransaction)
