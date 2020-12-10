c4f95c76e2adad82bfa862af36c615ada6ef67b7
Stanisław Światłoch <sswiatloch@hotmail.com>
2020-12-10 19:24:14 +0100

    Added signals to automatically update/create Profile

--- bank_backend/core/models.py
+++ bank_backend/core/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 
@@ -8,6 +10,16 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     private_key = models.CharField(max_length=52)  # WIF format
     wallet_address = models.CharField(max_length=34)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
 
 
 class SubAccount(models.Model):
