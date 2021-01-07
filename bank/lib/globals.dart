46103c8756f98bff4b71047c4d19edd007c36125
ondrikk <fajnygo@yahoo.pl>
2021-01-07 20:10:47 +0100

    Moved logged-in menu to globals

---
+++ bank/lib/globals.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:http/http.dart' as http;
+
+class Menu extends StatelessWidget {
+
+	Menu({
+    Key key,
+	this.auth_token
+  }) : super(key: key);
+
+  final String auth_token;
+
+  @override
+  Widget build(BuildContext context) {
+	var screenSize = MediaQuery.of(context).size;
+	  
+    return Container(
+            color: Colors.blue,
+            child: Padding(
+              padding: EdgeInsets.all(20),
+              child: Row(
+                children: [
+                  Text('WBB',
+                      style: GoogleFonts.montserrat(
+                          fontSize: 15,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.white)),
+                  //SizedBox(width: screenSize.width / 50),
+                  Expanded(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        InkWell(
+                          onTap: () {},
+                          child: Text('Przelew',
+                              style: GoogleFonts.montserrat(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.white)),
+                        ),
+                        SizedBox(width: screenSize.width / 50),
+                        InkWell(
+                          onTap: () {},
+                          child: Text('Lokata',
+                              style: GoogleFonts.montserrat(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.white)),
+                        ),
+                        SizedBox(width: screenSize.width / 50),
+                        InkWell(
+                          onTap: () {},
+                          child: Text('Rachunki',
+                              style: GoogleFonts.montserrat(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.white)),
+                        ),
+                        SizedBox(width: screenSize.width / 50),
+                        InkWell(
+                          onTap: () {},
+                          child: Text('Odbiorcy',
+                              style: GoogleFonts.montserrat(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.white)),
+                        ),
+                        SizedBox(width: screenSize.width / 50),
+                        InkWell(
+                          onTap: () {},
+                          child: Text('Historia',
+                              style: GoogleFonts.montserrat(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.white)),
+                        ),
+                        SizedBox(width: screenSize.width / 50),
+                        InkWell(
+                          onTap: () {},
+                          child: Text('Kontakt',
+                              style: GoogleFonts.montserrat(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.white)),
+                        ),
+                        SizedBox(width: screenSize.width / 50),
+                        InkWell(
+                          onTap: () {},
+                          child: Text('Zdarzenia',
+                              style: GoogleFonts.montserrat(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.white)),
+                        ),
+                      ],
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () {},
+                    child: Text('Ustawienia',
+                        style: GoogleFonts.montserrat(
+                            fontSize: 15,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.white)),
+                  ),
+                  SizedBox(
+                    width: screenSize.width / 50,
+                  ),
+                  InkWell(
+                    onTap: () {
+						http.post(
+							'http://localhost:8000/auth/token/logout',
+							headers: <String, String>{
+								'Authorization': 'Token ${auth_token}',
+							});
+						Navigator.of(context).pushNamed('/main');
+					},
+                    child: Text('Wyloguj',
+                        style: GoogleFonts.montserrat(
+                            fontSize: 15,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.white)),
+                  ),
+                ],
+              ),
+            ),
+        );
+  }
+}
--- bank/lib/logged_in.dart
+++ bank/lib/logged_in.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:http/http.dart' as http;
 import 'main.dart';
+import 'globals.dart';
 
 void main() => runApp(LoggedInApp());
 
@@ -40,117 +41,7 @@ class _HomePageState extends State<HomePage> {
         //extendBodyBehindAppBar: true,
         appBar: PreferredSize(
           preferredSize: Size(screenSize.width, 1000),
-          child: Container(
-            color: Colors.blue,
-            child: Padding(
-              padding: EdgeInsets.all(20),
-              child: Row(
-                children: [
-                  Text('WBB',
-                      style: GoogleFonts.montserrat(
-                          fontSize: 15,
-                          fontWeight: FontWeight.w500,
-                          color: Colors.white)),
-                  //SizedBox(width: screenSize.width / 50),
-                  Expanded(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        InkWell(
-                          onTap: () {},
-                          child: Text('Przelew',
-                              style: GoogleFonts.montserrat(
-                                  fontSize: 15,
-                                  fontWeight: FontWeight.w500,
-                                  color: Colors.white)),
-                        ),
-                        SizedBox(width: screenSize.width / 50),
-                        InkWell(
-                          onTap: () {},
-                          child: Text('Lokata',
-                              style: GoogleFonts.montserrat(
-                                  fontSize: 15,
-                                  fontWeight: FontWeight.w500,
-                                  color: Colors.white)),
-                        ),
-                        SizedBox(width: screenSize.width / 50),
-                        InkWell(
-                          onTap: () {},
-                          child: Text('Rachunki',
-                              style: GoogleFonts.montserrat(
-                                  fontSize: 15,
-                                  fontWeight: FontWeight.w500,
-                                  color: Colors.white)),
-                        ),
-                        SizedBox(width: screenSize.width / 50),
-                        InkWell(
-                          onTap: () {},
-                          child: Text('Odbiorcy',
-                              style: GoogleFonts.montserrat(
-                                  fontSize: 15,
-                                  fontWeight: FontWeight.w500,
-                                  color: Colors.white)),
-                        ),
-                        SizedBox(width: screenSize.width / 50),
-                        InkWell(
-                          onTap: () {},
-                          child: Text('Historia',
-                              style: GoogleFonts.montserrat(
-                                  fontSize: 15,
-                                  fontWeight: FontWeight.w500,
-                                  color: Colors.white)),
-                        ),
-                        SizedBox(width: screenSize.width / 50),
-                        InkWell(
-                          onTap: () {},
-                          child: Text('Kontakt',
-                              style: GoogleFonts.montserrat(
-                                  fontSize: 15,
-                                  fontWeight: FontWeight.w500,
-                                  color: Colors.white)),
-                        ),
-                        SizedBox(width: screenSize.width / 50),
-                        InkWell(
-                          onTap: () {},
-                          child: Text('Zdarzenia',
-                              style: GoogleFonts.montserrat(
-                                  fontSize: 15,
-                                  fontWeight: FontWeight.w500,
-                                  color: Colors.white)),
-                        ),
-                      ],
-                    ),
-                  ),
-                  InkWell(
-                    onTap: () {},
-                    child: Text('Ustawienia',
-                        style: GoogleFonts.montserrat(
-                            fontSize: 15,
-                            fontWeight: FontWeight.w500,
-                            color: Colors.white)),
-                  ),
-                  SizedBox(
-                    width: screenSize.width / 50,
-                  ),
-                  InkWell(
-                    onTap: () {
-						http.post(
-							'http://localhost:8000/auth/token/logout',
-							headers: <String, String>{
-								'Authorization': 'Token ${auth_token}',
-							});
-						Navigator.of(context).pushNamed('/main');
-					},
-                    child: Text('Wyloguj',
-                        style: GoogleFonts.montserrat(
-                            fontSize: 15,
-                            fontWeight: FontWeight.w500,
-                            color: Colors.white)),
-                  ),
-                ],
-              ),
-            ),
-          ),
+          child: Menu(auth_token: auth_token),
         ),
         body: Center(
           child: Container(
