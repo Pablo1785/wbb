90c92e05b2c7b151951ecdade35413d2f70844f6
Pawlacz B <pablo1785@wp.pl>
2021-01-09 20:32:57 +0100

    Cleanup of dangling class uses

--- bank_backend/core/admin.py
+++ bank_backend/core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Profile, SubAccount, BankDeposit, Transaction, BitcoinTransaction
+from .models import Profile, SubAccount, BankDeposit, Transaction
 
 # Register your models here.
 admin.site.register(Profile)
