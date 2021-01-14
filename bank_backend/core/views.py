088bdec29d44cfd13100d8d7f128a525fca46c6e
Pawlacz B <pablo1785@wp.pl>
2021-01-14 19:50:27 +0100

    User views now reachable, SubAccount API functioning

--- bank_backend/core/views.py
+++ bank_backend/core/views.py
@@ -39,21 +39,21 @@ class UserDetailView(APIView):
         except User.DoesNotExist:
             raise Http404
 
-    def get(self, request, pk, format=None):
-        user = self.get_object(pk)
+    def get(self, request, format=None):
+        user = self.get_object(request)
         user = UserSerializer(user)
         return Response(user.data)
 
-    def put(self, request, pk, format=None):
-        user = self.get_object(pk)
+    def put(self, request, format=None):
+        user = self.get_object(request)
         serializer = UserSerializer(user, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request, pk, format=None):
-        user = self.get_object(pk)
+    def delete(self, request, format=None):
+        user = self.get_object(request)
         user.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
@@ -69,14 +69,15 @@ class SubAccountListView(APIView):
         return Response(serializer.data)
 
     def post(self, request, format=None):
+        request.data['owner'] = request.user.id
         serializer = SubAccountSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request, pk, format=None):
-        subaccount = self.get_object(pk)
+    def delete(self, request, format=None):
+        subaccount = self.get_object(request)
         subaccount.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
@@ -86,7 +87,7 @@ class SubAccountDetailView(APIView):
     Retrieve, update or delete a subaccount instance.
     """
 
-    def get_object(self, pk):
+    def get_object(self, request):
         try:
             return SubAccount.objects.get(pk=pk)
         except SubAccount.DoesNotExist:
