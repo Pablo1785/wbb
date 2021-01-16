ed7e0b4d70e5c652f5e9ad20db9c8df9b348218c
Pawlacz B <pablo1785@wp.pl>
2021-01-16 19:25:49 +0100

    Added Requestor class for handling api requests. Added Subaccount requests. Added TokenData class for reading token responses.

--- bank/lib/api_requests.dart
+++ bank/lib/api_requests.dart
@@ -1,4 +1,10 @@
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+import 'package:bank/models.dart';
+
 const String server_port = '8000';
+const String server_address = 'http://127.0.0.1';
 
 //Sending data to server and getting response:
 Future<UserProfile> createUser(
@@ -21,3 +27,84 @@ Future<UserProfile> createUser(
     throw Exception('Błąd przy tworzeniu konta.\n\n${response.body}');
   }
 }
+
+class TokenData {
+    TokenData({
+        this.refresh,
+        this.access,
+    });
+
+    String refresh;
+    String access;
+
+    factory TokenData.fromRawJson(String str) => TokenData.fromJson(json.decode(str));
+
+    String toRawJson() => json.encode(toJson());
+
+    factory TokenData.fromJson(Map<String, dynamic> json) => TokenData(
+        refresh: json["refresh"] == null ? null : json["refresh"],
+        access: json["access"] == null ? null : json["access"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "refresh": refresh == null ? null : refresh,
+        "access": access == null ? null : access,
+    };
+}
+
+class Requestor {
+  final String serverAddress;
+  final String serverPort;
+
+  // Ths class doesn't handle logging in, it should be initialized with atuhToken and refreshToken on first login
+  String authToken;
+  String refreshToken;
+
+  Requestor(String authToken, String refreshToken, {this.serverAddress = 'http://127.0.0.1', this.serverPort = '8000'}) {
+    this.authToken = authToken;
+    this.refreshToken = refreshToken;
+  }
+
+  Future<TokenData> login(String username, String password) {
+    
+  }
+
+
+  // Subaccount creation:
+  Future<SubAccount> createSubaccount(String currency) async {
+    final http.Response response = await http.post(
+      '${this.serverAddress}:${this.serverPort}/api/subacc/',
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization': 'JWT ${this.authToken}'
+      },
+      body: jsonEncode(<String, String>{
+        'currency': currency,
+      }),
+    );
+
+    if (response.statusCode == 201) {
+      return SubAccount.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Błąd przy tworzeniu rachunku.\n\n${response.body}');
+    }
+  }
+
+  Future<List<SubAccount>> fetchSubaccounts() async {
+    final http.Response response = await http.post(
+      '${this.serverAddress}:${this.serverPort}/api/subacc/',
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization': 'JWT ${this.authToken}'
+      },
+    );
+
+    if (response.statusCode == 200 || response.statusCode == 204) {
+      List<Map<String, dynamic>> results = jsonDecode(response.body);
+      return List.from(results.map((model) => SubAccount.fromJson(model)));
+    } else {
+      throw Exception('Błąd przy pobieraniu rachunków.\n\n${response.body}');
+    }
+  }
+}
+
